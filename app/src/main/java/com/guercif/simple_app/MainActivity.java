b0f76be86a1d144c475a7acad101fcb44beb4f61
sadikfattah <sadik.fattah@outlook.com>
2022-02-08 09:49:17 +0100

    test

---
+++ app/src/main/java/com/guercif/simple_app/MainActivity.java
@@ -0,0 +1,14 @@
+package com.guercif.simple_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
